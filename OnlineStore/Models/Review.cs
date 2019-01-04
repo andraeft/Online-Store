10bb1b04404f5f99d343da66c89303188ab3c41c
Andra Eftene <andraeftene@gmail.com>
2019-01-05 01:06:57 +0200

    copiat mvc vechi

---
+++ OnlineStore/Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        [Required]
+        public String Comment { get; set; }
+    }
+
+}
